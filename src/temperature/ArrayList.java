45d2b6898f85e92a5fc00af29fac00cb4b5d21de
Amanda Boitano <amandamoreno@amandas-MacBook-Air.local>
2018-05-14 18:13:40 -0500

    first

---
+++ src/temperature/ArrayList.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temperature;
+
+import java.util.List;
+
+/**
+ *
+ * @author amandamoreno
+ */
+class ArrayList<T> {
+
+    ArrayList(List<Integer> asList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int indexOf(Integer tempInput) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    long[] toArray() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
